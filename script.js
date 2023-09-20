d990d6e8caef7df78a2ae0f50af5241847dce4a7
lindsayagreen <lindsgree@gmail.com>
2023-09-20 11:12:49 -0500

    added function

--- script.js
+++ script.js
@@ -4,14 +4,26 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  // console.log("Hey! You clicked the button!");
+
+
+
+
+
+
+  return "Generated Password";
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
