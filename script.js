22c06d12aa30b69e10c026393577465dbff3694f
lindsayagreen <lindsgree@gmail.com>
2023-09-20 22:38:12 -0500

    maybe fixed generated password?

--- script.js
+++ script.js
@@ -74,10 +74,9 @@ return generatedPassword;
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
 }
-
+  
+passwordText.value = password;
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
